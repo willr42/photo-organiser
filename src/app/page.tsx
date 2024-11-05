a68e8046abf1e11e36ea548e0d1e9143c0340d4c
Will Robertson <williamdrobertson42@gmail.com>
2024-11-05 18:23:18 +1100

    Install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+const photosEnv = process.env.PHOTOS_ROOT_DIR
+
 export default function Home() {
-  return <div></div>
+  if (!photosEnv) {
+    return (
+      <div>
+        <h1>
+          Please provide the root directory of your photos as an environment
+          variable.
+        </h1>
+      </div>
+    )
+  }
 }
