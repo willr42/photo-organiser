8d4e9af556ff5deecc9f0ac87f608dcf81107258
Will Robertson <williamdrobertson42@gmail.com>
2024-11-05 19:05:50 +1100

    Handle access errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import RefreshButton from "@/components/elements/RefreshButton"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { CircleX } from "lucide-react"
+import fs from "node:fs/promises"
+import path from "node:path"
 
 const photosEnv = process.env.PHOTOS_ROOT_DIR
 
-export default function Home() {
+export default async function Home() {
   if (!photosEnv) {
     return (
       <main className="flex h-screen items-center justify-center">
@@ -24,4 +26,38 @@ export default function Home() {
       </main>
     )
   }
+
+  const photosRootParsedPath = path.parse(photosEnv as string)
+  try {
+    // Descriptive var name
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const photosIsAccessible = await fs.access(
+      path.format(photosRootParsedPath),
+      fs.constants.R_OK,
+    )
+  } catch {
+    return (
+      <main className="flex h-screen items-center justify-center">
+        <Alert className="w-fit text-red-700">
+          <CircleX className="size-4 stroke-red-700" />
+          <AlertTitle className="font-bold">
+            Problem accessing directory
+          </AlertTitle>
+          <AlertDescription>
+            We could not access the
+            <code className="rounded-md bg-gray-100 p-1">PHOTOS_ROOT_DIR</code>.
+            Please check the path
+            <br></br> and restart the NextJS server.
+          </AlertDescription>
+          <RefreshButton />
+        </Alert>
+      </main>
+    )
+  }
+
+  return (
+    <main>
+      <div>{JSON.stringify(photosRootParsedPath)}</div>
+    </main>
+  )
 }
