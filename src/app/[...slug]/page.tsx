c928a37c292bf4e3edd909c67080a9ae9b8107bf
Will Robertson <williamdrobertson42@gmail.com>
2024-11-08 21:06:47 +1100

    Add file handling, will need server func here

--- src/app/[...slug]/page.tsx
+++ src/app/[...slug]/page.tsx
@@ -6,14 +6,32 @@ import { FileGrid } from "@/components/elements/FileGrid"
 export default async function Page({ params }: { params: { slug: string[] } }) {
   const { slug } = await params
   const currentPath = path.parse(path.join(WORKING_DIR, ...slug))
-  const dirContents = await fs.readdir(path.format(currentPath), {
-    withFileTypes: true,
-    recursive: true,
-  })
+  const DisplayElement = async () => {
+    const stats = await fs.stat(path.format(currentPath))
+    const isFile = stats.isFile()
+
+    if (isFile) {
+      return (
+        <div>
+          <img src={path.format(currentPath)} alt="Image" />
+        </div>
+      )
+    }
+
+    const isDir = stats.isDirectory()
+
+    if (isDir) {
+      const dirContents = await fs.readdir(path.format(currentPath), {
+        withFileTypes: true,
+        recursive: true,
+      })
+      return <FileGrid contents={dirContents} />
+    }
+  }
 
   return (
     <div className="flex flex-col p-6">
-      <FileGrid contents={dirContents} />
+      <DisplayElement />
     </div>
   )
 }
