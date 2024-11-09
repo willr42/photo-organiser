aef316bd748a69d97b9924dfb9afd0e535b15d4d
Will Robertson <williamdrobertson42@gmail.com>
2024-11-09 11:15:09 +1100

    Filter out _a & _b in grid

--- src/components/elements/FileGrid.tsx
+++ src/components/elements/FileGrid.tsx
@@ -5,9 +5,20 @@ import { Dirent } from "node:fs"
 import path from "node:path"
 
 export function FileGrid({ contents }: { contents: Dirent[] }) {
+  //   Filter out _a & _b files
+  const contentsNoDupes = contents
+    .map((dirent) => {
+      if (!dirent.isFile()) return dirent
+      const isSupplemental =
+        dirent.name.endsWith("_a") || dirent.name.endsWith("_b")
+      if (isSupplemental) return undefined
+      return dirent
+    })
+    .filter((dirent) => !!dirent)
+
   return (
     <div className="grid grid-cols-4 gap-4">
-      {contents.map((dirent) => {
+      {contentsNoDupes.map((dirent) => {
         const relativeFromRootPath = path.join(
           dirent.parentPath.replace(WORKING_DIR, ""),
           dirent.name,
