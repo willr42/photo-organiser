9ca06db6491f15d770354c8dcc11a5f4266e9635
Will Robertson <williamdrobertson42@gmail.com>
2024-11-16 17:59:45 +1100

    Add some formatting

--- src/components/elements/PhotoDisplay.tsx
+++ src/components/elements/PhotoDisplay.tsx
@@ -6,24 +6,32 @@ import { Button } from "@/components/ui/button"
 type Props = {
   front: string
   back: string | false
+  metadata?: string
 }
 
-export function PhotoDisplay({ front, back }: Props) {
+export function PhotoDisplay({ front, back, metadata }: Props) {
   const [activePhoto, setActivePhoto] = useState(front)
   return (
-    <div className="flex flex-col gap-4">
-      {back && (
-        <div className="flex gap-2">
-          <Button variant="outline" onClick={() => setActivePhoto(back)}>
-            Back
-          </Button>
-          <Button variant="outline" onClick={() => setActivePhoto(front)}>
-            Front
-          </Button>
-        </div>
-      )}
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={`/api${activePhoto}`} alt="Image" className="max-w-96" />
-    </div>
+    <main className="grid grid-cols-2">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl font-semibold">Image display</h1>
+        {back && (
+          <div className="flex gap-2">
+            <Button variant="outline" onClick={() => setActivePhoto(back)}>
+              Back
+            </Button>
+            <Button variant="outline" onClick={() => setActivePhoto(front)}>
+              Front
+            </Button>
+          </div>
+        )}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src={`/api${activePhoto}`} alt="Image" className="max-w-96" />
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold">Metadata</h2>
+        <code>{metadata}</code>
+      </div>
+    </main>
   )
 }
