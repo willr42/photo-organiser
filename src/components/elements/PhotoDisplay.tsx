f75e78b35165da170bbfd4772496263ce92646fc
Will Robertson <williamdrobertson42@gmail.com>
2024-11-17 20:53:14 +1100

    Show filename

--- src/components/elements/PhotoDisplay.tsx
+++ src/components/elements/PhotoDisplay.tsx
@@ -11,6 +11,7 @@ type Props = {
 
 export function PhotoDisplay({ front, back, metadata }: Props) {
   const [activePhoto, setActivePhoto] = useState(front)
+  const filename = front.split("/").pop()
   return (
     <main className="grid grid-cols-2">
       <div className="flex flex-col gap-4">
@@ -26,7 +27,13 @@ export function PhotoDisplay({ front, back, metadata }: Props) {
           </div>
         )}
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={`/api${activePhoto}`} alt="Image" className="max-w-96" />
+        <figure>
+          <img src={`/api${activePhoto}`} alt="Image" className="max-w-96" />
+          <figcaption className="mt-2 flex flex-col">
+            <span className="text-lg font-semibold">Path</span>
+            {filename}
+          </figcaption>
+        </figure>
       </div>
       <div>
         <h2 className="text-2xl font-semibold">Metadata</h2>
