bd036d52056ecb329556344130a4486bedcee80f
William Robertson <williamdrobertson42@gmail.com>
2024-11-26 15:26:35 +1100

    Prettify photo card (somewhat)

--- src/components/elements/ApplyDisplay.tsx
+++ src/components/elements/ApplyDisplay.tsx
@@ -15,26 +15,29 @@ export function ApplyDisplay({ workingDir }: { workingDir: string }) {
   const metadataArr = Object.entries(metadata)
   return (
     <div className="grid grid-cols-4 p-6">
-      {metadataArr.map(([filePath, timeStamp]) => (
-        <Popover key={filePath}>
-          <div className="flex flex-col">
-            <span>{filePath}</span>
-            {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src={`/api${workingDir + filePath}`} alt=""></img>
-            <p>Taken: {timeStamp}</p>
-            <div>
-              <PopoverTrigger
-                className={buttonVariants({ className: "inline" })}
-              >
-                Edit <Pencil />
-              </PopoverTrigger>
-              <PopoverContent>
-                <MetadataForm path={filePath} />
-              </PopoverContent>
+      {metadataArr.map(([filePath, timeStamp]) => {
+        const takenTimestamp = new Date(timeStamp).toDateString()
+        return (
+          <Popover key={filePath}>
+            <div className="flex flex-col gap-2 rounded-md border border-gray-300 p-4">
+              <span className="text-lg font-bold">{filePath}</span>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img src={`/api${workingDir + filePath}`} alt=""></img>
+              <p>Taken: {takenTimestamp}</p>
+              <div>
+                <PopoverTrigger
+                  className={buttonVariants({ className: "inline" })}
+                >
+                  Edit <Pencil />
+                </PopoverTrigger>
+                <PopoverContent>
+                  <MetadataForm path={filePath} />
+                </PopoverContent>
+              </div>
             </div>
-          </div>
-        </Popover>
-      ))}
+          </Popover>
+        )
+      })}
     </div>
   )
 }
