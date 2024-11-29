4f6e3cd8cf8a6a4a2781bffcd28a1906ade15cc3
William Robertson <williamdrobertson42@gmail.com>
2024-11-29 17:39:13 +1100

    Early return

--- src/components/elements/ApplyDisplay.tsx
+++ src/components/elements/ApplyDisplay.tsx
@@ -15,49 +15,50 @@ export function ApplyDisplay({ workingDir }: { workingDir: string }) {
   const router = useRouter()
   const { metadata } = useMetadataContext()
   const metadataArr = Object.entries(metadata)
+
+  if (metadataArr.length <= 0) {
+    return (
+      <div className="text-center">
+        <p className="mb-2 text-lg font-semibold">
+          No metadata to display, add some and come back.
+        </p>
+        <Button variant="outline" onClick={() => router.back()}>
+          Back
+        </Button>
+      </div>
+    )
+  }
+
   return (
     <div>
-      {metadataArr.length > 0 ? (
-        <div className="grid grid-cols-4 p-6">
-          {metadataArr.map(([filePath, timeStamp]) => {
-            const takenTimestamp = new Date(timeStamp).toDateString()
-            return (
-              <Popover key={filePath}>
-                <div className="flex flex-col gap-2 rounded-md border border-gray-300 p-4">
-                  <span className="text-lg font-bold">{filePath}</span>
-                  {/* eslint-disable-next-line @next/next/no-img-element */}
-                  <img src={`/api${workingDir + filePath}`} alt=""></img>
-                  <p>Taken: {takenTimestamp}</p>
-                  <div>
-                    <PopoverTrigger
-                      className={buttonVariants({ className: "inline" })}
-                    >
-                      Edit <Pencil />
-                    </PopoverTrigger>
-                    <PopoverContent>
-                      <MetadataForm path={filePath} />
-                    </PopoverContent>
-                  </div>
+      <div className="grid grid-cols-4 p-6">
+        {metadataArr.map(([filePath, timeStamp]) => {
+          const takenTimestamp = new Date(timeStamp).toDateString()
+          return (
+            <Popover key={filePath}>
+              <div className="flex flex-col gap-2 rounded-md border border-gray-300 p-4">
+                <span className="text-lg font-bold">{filePath}</span>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={`/api${workingDir + filePath}`} alt=""></img>
+                <p>Taken: {takenTimestamp}</p>
+                <div>
+                  <PopoverTrigger
+                    className={buttonVariants({ className: "inline" })}
+                  >
+                    Edit <Pencil />
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <MetadataForm path={filePath} />
+                  </PopoverContent>
                 </div>
-              </Popover>
-            )
-          })}
-        </div>
-      ) : (
-        <div className="text-center">
-          <p className="mb-2 text-lg font-semibold">
-            No metadata to display, add some and come back.
-          </p>
-          <Button variant="outline" onClick={() => router.back()}>
-            Back
-          </Button>
-        </div>
-      )}
+              </div>
+            </Popover>
+          )
+        })}
+      </div>
 
       <div className="mr-20 flex justify-end gap-2">
-        {metadataArr.length > 0 && (
-          <Button variant="outline">Apply changes?</Button>
-        )}
+        <Button variant="outline">Apply changes?</Button>
       </div>
     </div>
   )
